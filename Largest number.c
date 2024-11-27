cf4b0cc2cc7c0cb1139614ab66ccc2c28091c9c9
dheeraj192006 <dheeraj992006@gmail.com>
2024-11-27 16:27:39 +0530

    Create Largest number.c

---
+++ Largest number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int largest (inta,intb,intc){
+  if(a>=b&&a>=c) return a;
+  if(b>=a&&b>=c) return b;
+  return c;
+}
+int main(){
+  int x,y,z;
+  printf("Enter three numbers:");
+  scanf("%d%d%d",&x,&y,&z));
+  return 0;
+}
